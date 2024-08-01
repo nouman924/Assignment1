61c285ce06f603e2ef9310b665bd8bda2195cb2e
nouman924 <mk428787177@gmail.com>
2024-08-01 09:32:09 -0700

    Add files via upload

---
+++ noman.py
@@ -0,0 +1,10 @@
+# 1.My name is nouman ghafoor
+# 2.I am twenty years old
+# 3.I completed my matric from the arqam school
+# 4.I completed my intermediate from superior college
+# 5.Now i apply for BS IT in NFC IE&FR
+# 6.I have keen interest in artifical intelligence
+# 7.Its a great pleasure for me became apart of PIAIC
+# 8.I love to playing cricket
+# 9.I want to be a good teacher
+# 10.My dream to become create a humans robot that self humanity
